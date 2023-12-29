a7d47a71670e2d10f3af96a25e389f1b64ab0764
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-29 11:26:55 +0530

    Jenkins shared library

---
+++ vars/ci.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent { node { label 'workstation' } }
+
+    stages {
+        stage('Compile') {
+            steps {
+                echo 'Compile'
+            }
+        }
+        stage('Test cases') {
+            steps {
+                echo 'Test cases'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Build'
+            }
+        }
+        stage('Release') {
+            steps {
+                echo 'Release'
+            }
+        }
+    }
+}
