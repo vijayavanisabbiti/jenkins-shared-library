5ecaff98bccdf61bf2bab26b229471d8e74d432e
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-30 18:55:51 +0530

    Jenkins IP Update

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,36 +1,48 @@
 def call() {
-    pipeline {
-        agent { node { label 'workstation' } }
-
-        stages {
-            stage('Compile') {
-                steps {
-                    echo 'Compile'
-                }
-            }
-            stage('Test cases') {
-                steps {
-                    echo 'Test cases'
-                }
-            }
-
-            stage('Integration Test cases') {
-                steps {
-                    echo 'Test cases'
-                }
-            }
-
-            stage('Build') {
-                steps {
-                    echo 'Build'
-                }
-            }
-            stage('Release Application') {
-                steps {
-                    echo 'Release'
-                }
+    node ('workstation') {
+        if(env.TAG_NAME ==~ ".*") {
+            env.branchName = env.TAG_NAME
+        } else {
+            env.branchName = env.BRANCH_NAME
+        }
+        stage('Code checkout') {
+            git branch: env.branchName, credentialsId: 'vijayavani', url: 'https://github.com/vijayavanisabbiti/expense-backend'
+        }
+        stage('Compile') {}
+        if(env.BRANCH_NAME == "main") {
+            stage('Build') {}
+        } else if(env.BRANCH_NAME ==~ "PR.*") {
+            stage('test cases') {}
+            stage('Integration Test cases') {}
+        } else if(env.TAG_NAME ==~ ".*") {
+            stage('Build') {}
+            stage('Release App') {}
+        } else {
+            stage('test cases') {}
+        }
+        /*    stage('Build') {}
+        stage('Test cases') {
+            steps {
+                echo 'Test cases'
             }
         }
+
+        stage('Integration Test cases') {
+            steps {
+                echo 'Test cases'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Build'
+            }
+        }
+        stage('Release Application') {
+            steps {
+                echo 'Release'
+            }
+        }*/
     }
 }
 
