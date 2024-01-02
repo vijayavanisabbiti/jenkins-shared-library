f4b23667a729d7cf8bdd8ffd7b07c77e320601c8
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-02 15:05:54 +0530

    CI Codecheckout pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,43 +1,47 @@
 def call() {
     node ('workstation') {
-        sh "find . | sed -e '1d | xargs rm -rf"
+
+        sh "find . | sed -e '1d' |xargs rm -rf"
 
         if(env.TAG_NAME ==~ ".*") {
             env.branch_name = "refs/tags/${env.TAG_NAME}"
         } else {
-            if(env.BRANCH_NAME ==~ "PR.*") {
+            if (env.BRANCH_NAME ==~ "PR-.*") {
                 env.branch_name = "${env.CHANGE_BRANCH}"
             } else {
                 env.branch_name = "${env.BRANCH_NAME}"
             }
         }
 
-        stage ('Code Checkout') {
+        stage('Code Checkout') {
+            //git branch: "${env.branchName}", url: 'https://github.com/raghudevopsb76/expense-backend'
             checkout scmGit(
                     branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/vijayavanisabbiti/expense-backend"]]
+                    userRemoteConfigs: [[url: "https://github.com/raghudevopsb76/${repo_name}"]]
             )
-
-            sh 'cat Jenkinsfile'
         }
 
-        stage('Compile') {
-            if(env.JOB_BASE_NAME ==~ "PR.*") {
-                sh 'echo PR'
-                stage('Test Cases') {}
-                stage('Integration Test Cases') {}
+        sh 'cat Jenkinsfile'
+    }
+
+    stage('Compile') {
+
+        if(env.JOB_BASE_NAME ==~ "PR.*") {
+            sh 'echo PR'
+            stage('Test Cases') {}
+            stage('Integration Test Cases') {}
             } else if(env.BRANCH_NAME == "main") {
-                sh 'echo main'
-                stage('Build') {}
+            sh 'echo main'
+            stage('Build') {}
             } else if(env.TAG_NAME ==~ ".*") {
-                sh 'echo TAG'
-                stage('Build') {}
-                stage('Release App') {}
+            sh 'echo TAG'
+            stage('Build') {}
+            stage('Release App') {}
             } else {
-                sh 'echo branch'
-                stage('Test Cases') {}
-            }
+            sh 'echo branch'
+            stage('Test Cases') {}
         }
     }
 }
+
 
