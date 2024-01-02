1ec718a63090b2670c8f5a56ecb4fd7311de741c
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-02 15:39:12 +0530

    CI Codecheckout pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,18 +2,17 @@ def call() {
 
     node ('workstation') {
 
+        sh "find ."
+        sh "find . | sed -e '1d' |xargs rm -rf"
+
         if(env.TAG_NAME ==~ ".*") {
             env.branch_name = "refs/tags/${env.TAG_NAME}"
         } else {
-            if (env.BRANCH_NAME ==~ "PR-.*") {
-                env.branch_name = "${env.CHANGE_BRANCH}"
-            } else {
-                env.branch_name = "${env.BRANCH_NAME}"
-            }
+            env.branch_name = "${env.BRANCH_NAME}"
         }
 
         stage('Code Checkout') {
-            //git branch: "${env.branchName}", url: 'https://github.com/raghudevopsb76/expense-backend'
+
             checkout scmGit(
                     branches: [[name: "${branch_name}"]],
                     userRemoteConfigs: [[url: "https://github.com/vijayavanisabbiti/${repo_name}"]]
@@ -31,7 +30,7 @@ def call() {
             }
         }
 
-        else if(env.JOB_BASE_NAME ==~ "PR.*") {
+        if(env.JOB_BASE_NAME ==~ "PR.*") {
             sh 'echo PR'
             stage('Test Cases') {}
             stage('Integration Test Cases') {}
