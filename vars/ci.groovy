4897f74d0632cd47a4d3c233680383f05e1091ad
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-05 16:12:08 +0530

    Prod and Golden AMI

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,25 +7,7 @@ def call() {
 
     node ('workstation') {
 
-        sh "find . | sed -e '1d' |xargs rm -rf"
-
-        if (env.TAG_NAME ==~ ".*") {
-            env.branch_name = "refs/tags/${env.TAG_NAME}"
-        } else {
-            if (env.BRANCH_NAME ==~ "PR-.*") {
-                env.branch_name = "${env.CHANGE_BRANCH}"
-            } else {
-                env.branch_name = "${env.BRANCH_NAME}"
-            }
-        }
-
-        stage('Code Checkout') {
-        checkout scmGit(
-                branches: [[name: "${branch_name}"]],
-                userRemoteConfigs: [[url: "https://github.com/vijayavanisabbiti/${repo_name}"]]
-        )
-            sh 'cat Jenkinsfile'
-        }
+        common.codeCheckout()
 
         if (app_type == "nodejs") {
             stage('Download Dependencies') {
---
+++ vars/common.groovy
@@ -0,0 +1,22 @@
+def codeCheckout () {
+
+    sh "find . | sed -e '1d' |xargs rm -rf"
+
+    if (env.TAG_NAME ==~ ".*") {
+        env.branch_name = "refs/tags/${env.TAG_NAME}"
+    } else {
+        if (env.BRANCH_NAME ==~ "PR-.*") {
+            env.branch_name = "${env.CHANGE_BRANCH}"
+        } else {
+            env.branch_name = "${env.BRANCH_NAME}"
+        }
+    }
+
+    stage('Code Checkout') {
+        checkout scmGit(
+                branches: [[name: "${branch_name}"]],
+                userRemoteConfigs: [[url: "https://github.com/vijayavanisabbiti/${repo_name}"]]
+        )
+        sh 'cat Jenkinsfile'
+    }
+}
